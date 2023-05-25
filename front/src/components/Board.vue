ec6ce471efef3a527fe5e35164202df50c0203f6
pebblip <h.tomoyuki@me.com>
2023-05-25 23:38:55 +0900

    Feature/components (#5)
    * components
    
    * icon
    
    ---------
    
    Co-authored-by: Tomoyuki Higuchi <higuchi@peaberryz.com>

--- front/src/components/Board.vue
+++ front/src/components/Board.vue
@@ -4,11 +4,11 @@
       <div class="grid">
         <div
           class="d-flex justify-center"
-          v-for="i in [1, 2, 3, 4, 5, 6, 7, 8]"
+          v-for="i in rows"
           :key="i"
           style="width: 100%"
         >
-          <Cell v-for="j in [1, 2, 3, 4, 5, 5, 6, 7, 8]" :key="j" />
+          <Cell v-for="j in cols" :key="j" :color="cellAt(i, j)" />
         </div>
       </div>
     </div>
@@ -16,27 +16,27 @@
 </template>
 
 <script lang="ts" setup>
-import { isUserWhitespacable } from "@babel/types";
+import { PropType } from "vue";
+import { StoneColor } from "@/modules/StoneColor";
 import Cell from "./Cell.vue";
 
 const props = defineProps({
   size: {
     type: Number,
-    default: 8,
+    required: true,
+  },
+  cells: {
+    type: Array as PropType<StoneColor[]>,
+    required: true,
   },
 });
 
-enum Status {
-  Black,
-  White,
-  Empty,
-}
+const rows = Array.from({ length: props.size }, (_, i) => i);
+const cols = Array.from({ length: props.size }, (_, i) => i);
 
-type Point = {
-  x: number;
-  y: number;
-  status: Status;
-};
+function cellAt(x: number, y: number): StoneColor {
+  return props.cells[x * y];
+}
 </script>
 
 <style scoped>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <board />
+  <board :size="size" :cells="cells" />
 </template>
 
 <script lang="ts" setup>
 import HelloWorld from "@/components/HelloWorld.vue";
 import Board from "@/components/Board.vue";
+import { StoneColor } from "@/modules/StoneColor";
+
+const size = 8;
+const cells: StoneColor[] = Array.from({ length: 8 * 8 }, (_, i) => "black");
 </script>
