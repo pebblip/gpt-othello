094a9fa62a7016fc13981e56cfa2d42089b69c59
Tomoyuki Higuchi <higuchi@peaberryz.com>
2023-05-25 23:36:34 +0900

    icon

--- front/src/layouts/default/AppBar.vue
+++ front/src/layouts/default/AppBar.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar :elevation="8">
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-      AIオセロ
+      <v-icon icon="mdi-checkbox-blank-circle" />
+      Webオセロ
+      <v-icon icon="mdi-checkbox-blank-circle-outline" />
     </v-app-bar-title>
   </v-app-bar>
 </template>
