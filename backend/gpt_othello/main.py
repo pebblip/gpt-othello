445988223491b7fda4caba57834862867ec277b3
Tomoyuki Higuchi <higuchi@peaberryz.com>
2023-05-28 18:37:19 +0900

    fix

--- backend/gpt_othello/main.py
+++ backend/gpt_othello/main.py
@@ -1,9 +1,14 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api import app as api
 
-app = FastAPI()
+_stage = os.getenv("STAGE", "dev")
+
+_root_path = '' if _stage == 'dev' else '/prod'
+
+app = FastAPI(root_path=_root_path)
 
 app.mount("/api", api)
 app.add_middleware(
